8f37f27a10a925f0eeb5b6960df1a4fbe188e4f2
Abdul Wasay <abdulwasay985@gmail.com>
2026-07-23 10:37:03 +0500

    fix exit code 127 load nvm environment explicitly

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 cd /home/ubuntu/AWS-SERVICES
-# Direct npm install chalaein
+
+# NVM aur Node path ko force load karne ke liye yeh 3 lines sabse zaroori hain
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# Ab npm bina kisi error ke chalega
 npm install --production
---unsafe-perm
